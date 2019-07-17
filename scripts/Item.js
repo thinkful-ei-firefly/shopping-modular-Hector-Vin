f97729e667fd193c0e3ebebf134be7f47d00f1de
Hector Valles <hmvalles75@yahoo.com>
2019-07-17 09:19:40 -0700

    drill3

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,7 +1,15 @@
 'use strict';
 
 const Item = (function () {
-  
-  return {};
+  const validateName = function(name) {
+    if (!name) throw TypeError('Name does not exist.');
+  };
+  const create = function(name) {
+    return {id: cuid(), name, checked: false};
+  };
+  return {
+    validateName,
+    create,
+  };
 
 }() );
